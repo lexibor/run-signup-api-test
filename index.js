0345c9a0d475ccfcd77e315e69f0550f7d106c49
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-04-10 10:51:01 -0500

    test4

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(async function() {
     console.log('Hello World');
 
 
-    $.get(`https://runsignup.com/Rest/races?api_key=${keys.run_SignUp}&api_secret=${keys.run_SignUp_secret}&format=json&events=T&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=F&distance_units=K&zipcode=78258&radius=20`).done(function(json)
+    $.get(`https://runsignup.com/Rest/races?api_key=${keys.run_SignUp_secret}&api_secret=abfb0602fffd5549ad769ea40f1a5234295e9695&format=xml&events=F&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=F&distance_units=K`).done(async function(json)
     {
         console.log("testingggggggg")
-        console.log(json.result);
+        console.log(await json.result);
     });
 
 });
