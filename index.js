ab422e9c8dd5b1bc7ceb263255c7a06d84163b4c
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-04-10 09:17:46 -0500

    test 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     console.log('Hello World');
 
 
-    $.get("https://runsignup.com/Rest/races?api_key=&api_secret=&format=json&events=T&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=F&distance_units=K&zipcode=78258&radius=20").done(function(json)
+    $.get(`https://runsignup.com/Rest/races?api_key=${keys.run_SignUp}&api_secret=${keys.run_SignUp_secret}&format=json&events=T&race_headings=F&race_links=F&include_waiver=F&include_multiple_waivers=F&include_event_days=F&include_extra_date_info=F&page=1&results_per_page=50&sort=name+ASC&start_date=today&only_partner_races=F&search_start_date_only=F&only_races_with_results=F&distance_units=K&zipcode=78258&radius=20`).done(function(json)
     {
         console.log("testingggggggg")
         console.log(json.result);
